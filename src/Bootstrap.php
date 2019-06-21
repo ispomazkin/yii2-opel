678711db31fc3d276a4792eb0f9c7a379412d02e
Помазкин Иван Сергеевич <pomazkinis@pa.local>
2019-06-21 15:29:18 +0500

    fixed routes

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -4,6 +4,7 @@ namespace ispomazkin\opel;
 
 use Yii;
 use yii\base\BootstrapInterface;
+use app\components\Helper;
 
 
 
@@ -14,12 +15,17 @@ class Bootstrap implements BootstrapInterface{
     //Метод, который вызывается автоматически при каждом запросе
     public function bootstrap($app)
     {
+        //НЕ удалять!
+        $module = $app->getModule('opel');
+        /* @var $module \ispomazkin\opel\Module */
+
         $routes = require 'Routes.php';
         //Правила маршрутизации
         $app->getUrlManager()->addRules($routes, false);
 
         //для совместимости с проектом zp24.shop
         $app->params['relative'] = false;
+
 
     }
 }
