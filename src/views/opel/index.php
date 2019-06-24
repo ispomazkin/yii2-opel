0c214f61b73b3d3ff5e88b0230889e7a7bf67cc3
Помазкин Иван Сергеевич <pomazkinis@pa.local>
2019-06-24 08:53:12 +0500

    even & odd in models

--- src/views/opel/index.php
+++ src/views/opel/index.php
@@ -13,22 +13,37 @@ use ispomazkin\opel\OpelAssetBundle;
 use yii\helpers\Html;
 use yii\helpers\Url;
 
+//even && odd
+foreach($data as $key=>$row)
+{
+    if ($key%2 === 0)
+        $even[]=$row;
+    else $odd[] = $row;
+}
 
 OpelAssetBundle::register($this);
 ?>
 <?=Html::tag('h1',$this->title)?>
 <table class="table table-bordered table-responsive">
     <tr>
-        <th>Модель</th>
+        <th colspan="2">Модель</th>
     </tr>
-    <?php foreach($data as $row):?>
+    <?php foreach($even as $key=>$row):?>
         <tr>
             <td><?=Html::a($row['model'],Url::to([
                     'opel/categories',
                     'model_url'=>$row['url']
                 ]),[
                     'title'=>$row['model']
-                ])?></td>
+                ])?>
+            </td>
+            <td><?=Html::a($odd[$key]['model'],Url::to([
+                    'opel/categories',
+                    'model_url'=>$odd[$key]['url']
+                ]),[
+                    'title'=>$odd[$key]['model']
+                ])?>
+            </td>
         </tr>
     <?php endforeach;?>
 </table>
